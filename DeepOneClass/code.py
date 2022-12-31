d85c10788ba7dce997a28773d2cc174e80c30639
firstlook <157238067+firstlook@users.noreply.github.com>
2022-12-31 05:53:45 +0000

    changes to gitignore

--- DeepOneClass/code.py
+++ DeepOneClass/code.py
@@ -4,4 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # dataset
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_da
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train = x_train.reshape(x_train.s
