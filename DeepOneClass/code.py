3c8d3671ba2bc1a7e5d88d416847525e7521ae54
firstlook <157238067+firstlook@users.noreply.github.com>
2023-01-01 20:35:13 +0000

    merge and squash dev into master

--- DeepOneClass/code.py
+++ DeepOneClass/code.py
@@ -6,4 +6,5 @@ import matplotlib.pyplot as plt
 # dataset
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-x_train = x_train.reshape(x_train.s
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_
