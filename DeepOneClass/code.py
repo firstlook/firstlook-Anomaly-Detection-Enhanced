49d3884941a2c6df755a0d711398b9c03c2abab5
firstlook <157238067+firstlook@users.noreply.github.com>
2023-01-03 06:31:24 +0000

    bump version to 0.0.5

--- DeepOneClass/code.py
+++ DeepOneClass/code.py
@@ -7,4 +7,6 @@ import matplotlib.pyplot as plt
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-x_test = x_test.reshape(x_
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+x_train = x_train.astype('float32') 
