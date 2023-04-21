9fcfd298de182dcb1cf9a83a9e3ae59f884cf81d
firstlook <157238067+firstlook@users.noreply.github.com>
2023-04-21 18:16:46 +0000

    Dokumentation

--- DeepOneClass/total.py
+++ DeepOneClass/total.py
@@ -29,3 +29,49 @@ def train(x_target, x_ref, y_ref, epoch_num):
     # mobile net読み込み, S network用
     mobile = MobileNetV2(include_top=True, input_shape=input_shape, alpha=alpha,
                          , weights='imagenet')
+
+    #最終層削除
+    mobile.layers.pop()
+
+    # 重みを固定
+    for layer in mobile.layers:
+        if layer.name == "block_13_expand": # "block5_conv1": for VGG16
+            break
+        else:
+            layer.trainable = False
+
+    model_t = Model(inputs=mobile.input,outputs=mobile.layers[-1].output)
+
+    # R network用　Sと重み共有
+    model_r = Network(inputs=model_t.input,
+                      outputs=model_t.output,
+                      name="shared_layer")
+
+    #Rに全結合層を付ける
+    prediction = Dense(classes, activation='softmax')(model_t.output)
+    model_r = Model(inputs=model_r.input,outputs=prediction)
+
+    #コンパイル
+    optimizer = SGD(lr=5e-5, decay=0.00005)
+    model_r.compile(optimizer=optimizer, loss="categorical_crossentropy")
+    model_t.compile(optimizer=optimizer, loss=original_loss)
+
+    model_t.summary()
+    model_r.summary()
+
+    print("x_target is",x_target.shape[0],'samples')
+    print("x_ref is",x_ref.shape[0],'samples')
+
+    ref_samples = np.arange(x_ref.shape[0])
+    loss, loss_c = [], []
+
+    print("training...")
+
+    #学習
+    for epochnumber in range(epoch_num):
+        x_r, y_r, lc, ld = [], [], [], []
+
+        #ターゲットデータシャッフル
+        np.random.shuffle(x_target)
+
+        #リファレンスデータシャッフル
