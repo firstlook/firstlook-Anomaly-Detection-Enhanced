8060ab094762473f385098f4d085bceb6b1cbb58
firstlook <157238067+firstlook@users.noreply.github.com>
2023-11-07 22:21:51 +0000

    v2.9.2

--- Mahalanobis/mahal_dist.py
+++ Mahalanobis/mahal_dist.py
@@ -16,4 +16,5 @@ def mahal_dist(matrix):
 
     # 求单个样本向量与样本中心的马氏距离
     def md_vector(vector):        
-        inner_prod =
+        inner_prod = np.dot(vector, cov_matrix_inv)
+        inner_product 
