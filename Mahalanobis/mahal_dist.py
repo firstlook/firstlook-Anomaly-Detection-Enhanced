5d304af840b77163bdca6815c2b99a95ed87c12f
firstlook <157238067+firstlook@users.noreply.github.com>
2023-11-11 05:52:07 +0000

    Project cleanup

--- Mahalanobis/mahal_dist.py
+++ Mahalanobis/mahal_dist.py
@@ -22,4 +22,5 @@ def mahal_dist(matrix):
         return dist
     
     # 求矩阵中所有样本与中心之间的马氏距离
-    mahal_dist = np.apply_along_axis(arr=delta, axis=
+    mahal_dist = np.apply_along_axis(arr=delta, axis=1, func1d=md_vector)
+    return mahal_dist
