5bb60e632612d0245ba54146bc59652435419959
firstlook <157238067+firstlook@users.noreply.github.com>
2023-11-11 11:28:30 +0000

    cookies

--- Mahalanobis/mahal_dist_variant.py
+++ Mahalanobis/mahal_dist_variant.py
@@ -7,4 +7,6 @@ from sklearn.preprocessing import StandardScaler
 
 def mahal_dist_variant(matrix):
     # 将数据集标准化
-    matrix = StandardScaler().fit_transform
+    matrix = StandardScaler().fit_transform(matrix)
+    # 对数据集进行主成分分析
+    cov_matrix = np.cov(matri
