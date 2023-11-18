b69d8414d8787d8059fa2a6c3e5895107591df60
firstlook <157238067+firstlook@users.noreply.github.com>
2023-11-18 19:53:01 +0000

    changed the readme

--- Mahalanobis/main.py
+++ Mahalanobis/main.py
@@ -14,4 +14,5 @@ parser.add_argument('--mahalanobis', dest='mahalanobis', action='store_true')
 parser.set_defaults(mahalanobis=False)
 parser.add_argument('--mahalanobis_cov_decay', type=float, default=1E-4)
 parser.add_argument('--distort_inputs', dest='distort_inputs',
-                    a
+                    action='store_true')
+parser.set_defaults(distort_in
