8db20cb100e080dfc7cb608c6f5252d17d242048
firstlook <157238067+firstlook@users.noreply.github.com>
2023-11-19 18:26:10 +0000

    cleanup and docs

--- Mahalanobis/main.py
+++ Mahalanobis/main.py
@@ -23,4 +23,5 @@ parser.set_defaults(distort_targets=False)
 # Dataset args
 parser.add_argument('--dataset_name', type=str, default='forest_cover',
                     help='name of the dataset')
-parser.add_argument('--
+parser.add_argument('--test_prop', type=str, default=0.2)
+parser.add_argument('
