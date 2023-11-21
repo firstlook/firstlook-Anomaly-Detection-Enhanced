9587d5a84fae27c5d0a4a2f941a60ab55239dab8
firstlook <157238067+firstlook@users.noreply.github.com>
2023-11-21 08:31:44 +0000

    Ditz issue update

--- Mahalanobis/main.py
+++ Mahalanobis/main.py
@@ -28,4 +28,5 @@ parser.add_argument('--val_prop', type=str, default=0.2)
 
 # Training args
 parser.add_argument('--n_epochs', type=int, default=500)
-parser.add_argument('--batch_size', type=int, defau
+parser.add_argument('--batch_size', type=int, default=512)
+parser.add_argument('--no_adam',  d
