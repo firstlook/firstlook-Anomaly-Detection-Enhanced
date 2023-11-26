bd970b8f24e6aff4d4b55126a5c04c78ce111361
firstlook <157238067+firstlook@users.noreply.github.com>
2023-11-26 05:50:18 +0000

    Forgot to add.

--- Mahalanobis/main.py
+++ Mahalanobis/main.py
@@ -35,4 +35,7 @@ parser.set_defaults(adam=True)
 parser.add_argument('--no_cuda', dest='cuda', action='store_false')
 parser.set_defaults(cuda=True)
 parser.add_argument('--no_tensorboard', dest='tensorboard', action='store_false')
-parser.set_defaults(tenso
+parser.set_defaults(tensorboard=True)
+
+# Collect args and kwargs
+args = parser.parse_ar
