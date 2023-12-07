b2939b9994b7ba1c2a09c2c6db487d8e56a79d06
firstlook <157238067+firstlook@users.noreply.github.com>
2023-12-07 09:29:41 +0000

    0.15.5

--- Mahalanobis/modules/autoencoder.py
+++ Mahalanobis/modules/autoencoder.py
@@ -5,4 +5,8 @@ Autoencoder module
 """
 import torch
 import torch.nn as nn
-from modules.mahalanobis import Mahalan
+from modules.mahalanobis import MahalanobisLayer
+
+class Autoencoder(nn.Module):
+
+    def __init__(s
