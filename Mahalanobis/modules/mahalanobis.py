4fae132935d246e130441c36a06e188ebc546b6f
firstlook <157238067+firstlook@users.noreply.github.com>
2023-12-19 21:59:04 +0000

    split files

--- Mahalanobis/modules/mahalanobis.py
+++ Mahalanobis/modules/mahalanobis.py
@@ -8,4 +8,5 @@ import torch.nn as nn
 
 class MahalanobisLayer(nn.Module):
 
-    def __init__(self, dim, decay = 0.1)
+    def __init__(self, dim, decay = 0.1):
+        super(MahalanobisLayer, self).
