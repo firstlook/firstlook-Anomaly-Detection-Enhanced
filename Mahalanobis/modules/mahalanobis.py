5e7ea6b86b7516d73013f23e40b2c14b8a3df30d
firstlook <157238067+firstlook@users.noreply.github.com>
2024-01-16 12:06:24 +0000

    remove img

--- Mahalanobis/modules/mahalanobis.py
+++ Mahalanobis/modules/mahalanobis.py
@@ -75,4 +75,5 @@ if __name__ == "__main__":
         mah_layer.update(X, X_fit)
     np.testing.assert_almost_equal(np_cov_delta, mah_layer.S.numpy())
 
-    # Test if numpy inverse and pytorch pseudo inverse are clos
+    # Test if numpy inverse and pytorch pseudo inverse are close
+    np.testing.assert_almost_equal(np.linalg.inv(np_cov
