254db3b7e9d7d7fb8e7320f3160e70f0399046b8
firstlook <157238067+firstlook@users.noreply.github.com>
2024-01-17 05:34:38 +0000

    add comment.

--- Mahalanobis/run.sh
+++ Mahalanobis/run.sh
@@ -5,4 +5,5 @@ source activate pytorch
 # Forest cover
 python3 main.py --dataset_name forest_cover
 python3 main.py --mahalanobis --dataset_name forest_cover
-python3 main.py --mahalanobis --di
+python3 main.py --mahalanobis --distort_inputs --dataset_name forest_cover
+python3 main
