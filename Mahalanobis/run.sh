bbbd6d0b305087add0f4e6cfd9262d16a65009b0
firstlook <157238067+firstlook@users.noreply.github.com>
2024-01-24 16:13:07 +0000

    tcp_output: set initial TCP window size to 64K (speed improvement)

--- Mahalanobis/run.sh
+++ Mahalanobis/run.sh
@@ -23,4 +23,5 @@ python3 main.py --mahalanobis --distort_targets --dataset_name kdd_http
 # Shuttle
 python3 main.py --dataset_name shuttle
 python3 main.py --mahalanobis --dataset_name shuttle
-python3 main.py --mahalanobis --distort_inputs --dataset_
+python3 main.py --mahalanobis --distort_inputs --dataset_name shuttle
+python3 main.py --mahalanobis --distort
