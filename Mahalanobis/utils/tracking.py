2570583f8a5f335627bdf8c3d058271ba4849927
firstlook <157238067+firstlook@users.noreply.github.com>
2024-02-13 05:51:50 +0000

    pwa

--- Mahalanobis/utils/tracking.py
+++ Mahalanobis/utils/tracking.py
@@ -62,4 +62,4 @@ class Tracker:
             self.writer.add_scalar('data/train_loss', train_loss, self.k)
             self.writer.add_scalar('data/val_loss', val_loss, self.k)
             self.writer.add_scalar('data/top1_percent', top1_percent, self.k)
-          
+            self.writer.add_scalar('data/top5_percent', top5_percent, 
