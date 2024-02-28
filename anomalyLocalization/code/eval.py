ac50677f58703fa3593db31e4b3d28e6ad8bd45c
firstlook <157238067+firstlook@users.noreply.github.com>
2024-02-28 06:25:24 +0000

    Use var

--- anomalyLocalization/code/eval.py
+++ anomalyLocalization/code/eval.py
@@ -22,3 +22,46 @@ def EBM(model,test_loader,device):
     recon_x = model(x_0).detach()
     loss = F.binary_cross_entropy(x_0, recon_x, reduction='sum')  
     loss.backward(retain_graph=True)
+
+    x_grad = x_0.grad.data
+    
+    x_t = x_0 - alpha * x_grad * (x_0 - recon_x) ** 2
+
+    for i in range(15):
+        recon_x = model(x_t).detach()
+        loss = F.binary_cross_entropy(x_t, recon_x, reduction='sum') + lamda * torch.abs(x_t - x_0).sum()
+        loss.backward(retain_graph=True)
+
+        x_grad = x_0.grad.data
+        #eps = 0.028
+        x_grad = F.normalize(x_grad)
+        eps = 0.4
+        x_t = x_t - eps * x_grad * (x_t - recon_x) ** 2
+        iterative_plot(x_t.detach().cpu().numpy(), i)
+
+        
+# gif
+def iterative_plot(x_t, j):
+    plt.figure(figsize=(15, 4))
+    for i in range(10):
+        plt.subplot(1, 10, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(x_t[i][0], cmap=plt.cm.gray)
+    plt.subplots_adjust(wspace=0., hspace=0.)        
+    plt.savefig("./results/{}.png".format(j))
+    #plt.show()
+    
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    seed = 42
+    out_dir = './logs'
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    checkpoints_dir ="./checkpoints"
+    if not os.path.exists(checkpoints_dir):
+        os.mkdir(out_dir)
+        
+    torch.manual_seed(seed)
