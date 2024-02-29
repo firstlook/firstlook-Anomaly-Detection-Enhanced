f8878ec9315d6c36909cc7aabfca184809281687
firstlook <157238067+firstlook@users.noreply.github.com>
2024-02-29 14:17:05 +0000

    credit where credit is due

--- anomalyLocalization/code/train.py
+++ anomalyLocalization/code/train.py
@@ -27,3 +27,47 @@ def eval(model,test_loader,device):
     with torch.no_grad():
         x_vae = model(x_0.to(device)).detach().cpu().numpy()
 
+
+def EBM(model,test_loader,device):
+    model.train()
+    x_0 = iter(test_loader).next()
+    alpha = 0.05
+    lamda = 1
+    x_0 = x_0.to(device).clone().detach().requires_grad_(True)
+    recon_x = model(x_0).detach()
+    loss = F.binary_cross_entropy(x_0, recon_x, reduction='sum')  
+    loss.backward(retain_graph=True)
+
+    x_grad = x_0.grad.data
+    x_t = x_0 - alpha * x_grad * (x_0 - recon_x) ** 2
+
+    for i in range(15):
+        recon_x = model(x_t).detach()
+        loss = F.binary_cross_entropy(x_t, recon_x, reduction='sum') + lamda * torch.abs(x_t - x_0).sum()
+        loss.backward(retain_graph=True)
+
+        x_grad = x_0.grad.data
+        eps = 0.001
+        x_t = x_t - eps * x_grad * (x_t - recon_x) ** 2
+        iterative_plot(x_t.detach().cpu().numpy(), i)
+
+        
+# gif
+def iterative_plot(x_t, j):
+    plt.figure(figsize=(15, 4))
+    for i in range(10):
+        plt.subplot(1, 10, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(x_t[i][0], cmap=plt.cm.gray)
+    plt.subplots_adjust(wspace=0., hspace=0.)        
+    plt.savefig("./results/{}.png".format(j))
+    #plt.show()
+    
+def main():
+    train_loader = return_MVTecAD_loader(image_dir="./mvtec_anomaly_detection/grid/train/good/", batch_size=256, train=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    seed = 42
