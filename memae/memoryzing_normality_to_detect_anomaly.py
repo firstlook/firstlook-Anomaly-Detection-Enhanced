d12a0264e59f93ebaea913cb019b8e72d18d3b80
firstlook <157238067+firstlook@users.noreply.github.com>
2024-03-05 19:34:53 +0000

    .gitignore changed

--- memae/memoryzing_normality_to_detect_anomaly.py
+++ memae/memoryzing_normality_to_detect_anomaly.py
@@ -21,3 +21,51 @@ from sklearn.metrics import f1_score, accuracy_score
 
 
 T.set_default_tensor_type('torch.FloatTensor')
+
+batch_size = 32
+nb_epochs  = 1
+nb_digits  = 10
+
+
+train_normals = [
+    img for img, lbl in datasets.MNIST(
+        './data', train=True, download=True,
+        transform=transforms.Compose([
+            transforms.ToTensor(), 
+            #transforms.Normalize((0.1307,), (0.3081,))
+        ])
+    ) if lbl == 9
+]
+train_normals = torch.utils.data.TensorDataset(
+    torch.tensor([v.numpy() for v in train_normals])
+)
+train_normals_loader = T.utils.data.DataLoader(
+    train_normals, 
+    batch_size=batch_size, 
+    shuffle=True
+) 
+
+
+train_loader = T.utils.data.DataLoader(datasets.MNIST(
+    './data', train=True, download=True,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+    ])),
+    batch_size=batch_size, shuffle=True
+)
+ 
+test_loader = T.utils.data.DataLoader(datasets.MNIST(
+    './data', train=False, download=True,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+    ])),
+    batch_size=batch_size, shuffle=False
+) 
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1,  16, 1, stride=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
